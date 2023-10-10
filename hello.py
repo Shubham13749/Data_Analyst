0a4852901e4fa6245fb82f959a52f00340b61793
Shubham Singh Rajput <Shubham13749@gmail.com>
2023-10-10 12:02:11 +0530

    First Commit

---
+++ hello.py
@@ -0,0 +1,33 @@
+# First = float(input("Enter the first Number: "))
+# Operator = input("Please Select the operator (+,-,*,/,%)")
+# Second = float(input("Enter the second Number: "))
+
+# if Operator == "+":
+#     print(First + Second)
+# elif Operator == "-":
+#     print(First - Second)
+# elif Operator == "*":
+#    print(First * Second)
+# elif Operator == "/":
+#     print(First / Second)
+# elif Operator == "%":
+#     print(First % Second)
+# else:
+#     print("invallid operation")
+
+"""Above is the code for calculator """
+
+# i = 0
+
+# while i <=10:
+#     print(i * "*")
+#     i = i +1
+
+i = 10
+while i>=0:
+    print(i * "*")
+    i=i-1
+
+     
+ 
+ 
